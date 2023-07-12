fc0ebe389e8d345d83538bcb3745061e1fa3fa13
Mustafa <mustafakhan6816899@gmail.com>
2023-07-12 18:25:26 +0500

    Initial commit

---
+++ affanuni.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void swap(int x[][2])
+{
+	
+	for(int i=1;i>0;i--)
+	{
+		for(int j=2;j>0;j--)
+		{
+			cout<<" the number after swap : "<<x[i][j]<<endl;
+		}
+	}
+}
+
+main()
+{
+	
+}
---
+++ love.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+	
+	 char (answer);
+	cout<<"Enter felling you love me or not('y'/'n') "<<"\n\n";
+	cin>>answer;
+	if(answer=='y')
+	{
+		cout<<" i love u too";
+	}
+	else
+	{
+	  cout<<"i hate you too";
+	}
+	return 0;
+}
